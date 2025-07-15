cf1815412df0523539c0be45ce1bda86ed89ebb7
lefv <luis@lefv.io>
2025-07-15 16:49:28 -0400

    Removed debug statement.

--- src/mcli/app/main.py
+++ src/mcli/app/main.py
@@ -13,8 +13,6 @@ import tomli
 import os
 from mcli.lib.logger.logger import get_logger, enable_runtime_tracing, disable_runtime_tracing
 
-print("[DEBUG] mcli.app.main loaded")
-
 # Get logger
 logger = get_logger()
 
